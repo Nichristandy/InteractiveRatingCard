2ace6ff961f241620bdf5ca6cebdee759331b579
Nichristandy <65210946+Nichristandy@users.noreply.github.com>
2023-08-02 15:46:39 +0700

    validation

--- script/index.js
+++ script/index.js
@@ -1,16 +1,27 @@
 
 var rate = $(".rate")
 var submit = $(".submit")
+var clicked = false
 
-submit.on("click", function(){
-    $("#rate").addClass("hidden")
-    $('#thankyou').removeClass("hidden")
-})
+
 
 rate.on("click",function(){
     rate.removeClass("active")
     var value = $(this).text();
     $(this).addClass("active"); 
     $(".rate-score").text("You Selected " + value + " out of 5") 
+    clicked = true;
 });
+
+submit.on("click", function(){
+    if(clicked == false){
+        alert("You should choose your rating")
+    }else if(clicked == true){
+        $("#rate").addClass("hidden")
+        $('#thankyou').removeClass("hidden")
+    }
+
+
+
+})
 
